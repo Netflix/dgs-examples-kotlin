b22fe60c84252fd6ff37b14369134eb86423f801
Andrew Stucky <andrew.david.stucky@gmail.com>
2022-12-07 17:33:21 -0500

    chore: removes comment I added for my own thought process when digging into the 'test-examples.py' innerworkings.

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,7 +41,6 @@ repositories {
 }
 
 dependencies {
-    // TODO: How does this get handled?...
     implementation(platform("com.netflix.graphql.dgs:graphql-dgs-platform-dependencies:latest.release"))
     implementation("com.netflix.graphql.dgs:graphql-dgs-spring-boot-starter")
     implementation("com.netflix.graphql.dgs:graphql-dgs-extended-scalars")
